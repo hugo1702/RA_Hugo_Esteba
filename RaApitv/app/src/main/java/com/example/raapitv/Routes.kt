9bc90d6ef386aad8bc41077ed722aa3606f651e6
hugo1702 <hugonorberton@gmail.com>
2024-08-22 13:29:18 -0600

    Se agrego el proyecto con la API TMDB

---
+++ RaApitv/app/src/main/java/com/example/raapitv/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.raapitv
+
+sealed class Routes (val route: String){
+    object AcercaDe: Routes("acercade")
+    object Menu: Routes("menu")
+    object MovieList: Routes("muvie_list")
+    object MovieDetailScreen: Routes("moviedeatilscreen")
+    /*object MovieDetailScreen : Routes("movie_detail/{movieId}")*/
+}
